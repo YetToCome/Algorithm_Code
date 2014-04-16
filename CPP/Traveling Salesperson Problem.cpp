2a8ef77fc56f2185af86494bd67d1410f6120f87
JiaWei Wang <YetToCome1992@gmail.com>
2014-04-16 17:01:20 +0800

    Update Traveling Salesperson Problem.cpp

--- CPP/Traveling Salesperson Problem.cpp
+++ CPP/Traveling Salesperson Problem.cpp
@@ -19,9 +19,36 @@ vector<vector<int>> MAP{{INF, 16 , 12 , 13 , 6  , 7  , 11},
                         {7  , 19 , 3  , 10 , 2  , INF, 9 },
                         {11 , 5  , 15 , 4  , 17 , 9  , INF}};
 
-vector<int> Nodes;
-multimap<int, pair<int, int>> Map_Node;
-list<pair<int, pair<int, int>>> Sorted_Map;
+vector<int> Nodes; //所有添加后的节点
+multimap<int, pair<int, int>> Map_Node;  //地图
+list<pair<int, pair<int, int>>> Sorted_Map; //排序好的地图
+vector<pair<pair<int, int>, int>> edges; //添加进来的边
+vector<int> path;  //路径
+
+bool three_times(vector<pair<pair<int, int>, int>>& edges, int key) {
+    int times = 0;
+    for (auto i = edges.begin(); i != edges.end(); ++i) {
+        if (i->first.first == key || i->first.second == key)
+            times++;
+    }
+    if (times >= 2)
+        return false;
+    else return true;
+}
+
+bool issmae(vector<pair<pair<int, int>, int>>& edges, pair<int, int> e) {
+    for (auto i = edges.begin(); i != edges.end(); ++i) {
+        if (i->first.first == e.second && i->first.second == e.first)
+            return true;
+    }
+    return false;
+}
+
+bool iscycle(vector<int>& path, pair<int, int> e) {
+    if (e.first == path.front() && e.second == path.back()) return true;
+    if (e.second == path.front() && e.first == path.back()) return true;
+    return false;
+}
 
 int main()
 {
@@ -34,19 +61,44 @@ int main()
     for (auto a = Map_Node.begin(); a != Map_Node.end() && a->first != INF; ++a) {
         Sorted_Map.push_back(make_pair(a->first, make_pair(a->second.first, a->second.second)));
     }
-    while (Nodes.size() != N) {
+    while (Nodes.size() <= N) {
         for (auto node = Sorted_Map.begin(); node != Sorted_Map.end(); ++ node) {
-            if (find(Nodes.begin(), Nodes.end(), node->second.first) == Nodes.end() || find(Nodes.begin(), Nodes.end(), node->second.second) == Nodes.end() )
-            if (Nodes.empty() || node->second.first == Nodes.back()) {
-                cout << node->second.first << "->" << node->second.second << " "<< node->first<<endl;
+            if (Nodes.empty()) {
+                cout << node->second.first << "->" << node->second.second<< " " <<node->first << endl;
+                Nodes.push_back(node->second.first); path.push_back(node->second.first);
+                Nodes.push_back(node->second.second); path.push_back(node->second.second);
+                edges.push_back(make_pair(make_pair(node->second.first, node->second.second), 1));
                 SUM += node->first;
-                if (Nodes.empty())
-                    Nodes.push_back(node->second.first);
-                Nodes.push_back(node->second.second);
-                node = Sorted_Map.erase(node);
+            } else if (!iscycle(path, make_pair(node->second.first, node->second.second))){
+                if (three_times(edges, node->second.first) && three_times(edges, node->second.second)) {
+                    if (!issmae(edges, make_pair(node->second.first, node->second.second))) {
+                        cout << node->second.first << "->" << node->second.second << " " <<node->first << endl;
+                        if (find(Nodes.begin(), Nodes.end(), node->second.first) == Nodes.end())
+                            Nodes.push_back(node->second.first);
+                        else
+                            Nodes.push_back(node->second.second);
+                        SUM += node->first;
+                        edges.push_back(make_pair(make_pair(node->second.first, node->second.second), 0));
+                        for (auto x = edges.begin(); x != edges.end(); ++ x) {
+                            if (x->second == 0 && x->first.first == path.back()) {
+                                path.push_back(x->first.first); path.push_back(x->first.second);
+                                x->second = 1;
+                            } else if (x->second == 0 && x->first.second == path.back()) {
+                                path.push_back(x->first.second); path.push_back(x->first.first);
+                                x->second = 1;
+                            }
+
+                        }
+                    }
+                }
             }
         }
     }
+    auto p = unique(path.begin(), path.end());
+    path.erase(p, path.end());
+    for (auto x : path)
+        cout << x << " ";
+    cout << endl;
     cout << SUM << endl;
     return 0;
 }
